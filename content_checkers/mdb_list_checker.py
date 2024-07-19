be226ed19186d19e7e9f5ca77dc1332f4ca57acb
godver3 <liam.d.hughes@gmail.com>
2024-07-19 17:54:19 +0000

    Removing

--- content_checkers/mdb_list_checker.py
+++
@@ -1,4 +0,0 @@
-
-async def check_mdb_list_requests():
-    # Mock function to simulate fetching requests from MDB List
-    pass
--- state_machine.py
+++
@@ -1,8 +0,0 @@
-
-class StateMachine:
-    def __init__(self):
-        self.state = "initial"
-
-    def transition(self, new_state):
-        # Add your state transition logic here
-        self.state = new_state
