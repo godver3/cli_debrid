062b4e0ac0dd0d1da9a2c26f7baa0385043f384c
mash2k3 <mash2k3@yahoo.com>
2024-08-09 21:51:02 -0400

    Added Jackett Instances

--- scraper/jackett.py
+++ scraper/jackett.py
@@ -3,68 +3,72 @@ import logging
 from typing import List, Dict, Any
 from database import get_title_by_imdb_id
 from metadata.metadata import get_year_from_imdb_id
-from settings import get_setting
+from settings import get_setting, get_jackett_settings
 from urllib.parse import quote, urlencode
 
-JACKETT_URL = get_setting('Jackett', 'url')
-JACKETT_API = get_setting('Jackett', 'api')
 JACKETT_FILTER = "!status:failing,test:passed"
-ENABLED_INDEXERS = get_setting('Jackett', 'enabled_indexers')
-JACKETT_ENABLED = get_setting('Jackett', 'enabled')
 
 def scrape_jackett(imdb_id: str, content_type: str, season: int = None, episode: int = None) -> List[Dict[str, Any]]:
-    if not JACKETT_ENABLED:
-        logging.debug("Jackett disabled")
-        return []
-
-    title = get_title_by_imdb_id(imdb_id)
-    if not title:
-        return []
-
-    year = get_year_from_imdb_id(imdb_id)
-    if not year:
-        return []
-
-    if content_type.lower() == 'movie':
-        params = f"{title} {year}"
-    else:
-        params = f"{title}"
-
-    if content_type.lower() == 'tv' and season is not None:
-        params = f"{params}.s{season:02d}"
-        if episode is not None:
-            params = f"{params}.e{episode:02d}"
-
-    search_endpoint = f"{JACKETT_URL}/api/v2.0/indexers/{JACKETT_FILTER}/results?apikey={JACKETT_API}"
-    
-    query_params = {'Query': params}
-    
-    if ENABLED_INDEXERS:
-        indexers = [indexer.strip().lower() for indexer in ENABLED_INDEXERS.split(',')]
-        query_params.update({f'Tracker[]': indexer for indexer in indexers})
-
-    full_url = f"{search_endpoint}&{urlencode(query_params, doseq=True)}"
-    
-    logging.debug(f"Attempting to access Jackett API with URL: {full_url}")
-
-    try:
-        response = requests.get(full_url, headers={'accept': 'application/json'})
-        logging.debug(f"Jackett API status code: {response.status_code}")
-        if response.status_code == 200:
-            try:
-                data = response.json()
-                return parse_jackett_results(data["Results"])
-            except requests.exceptions.JSONDecodeError as json_error:
-                logging.error(f"Failed to parse JSON response: {str(json_error)}")
-                return []
-        else:
-            logging.error(f"Jackett API error: Status code {response.status_code}")
+    jackett_results=[]
+    jackett_instances = get_jackett_settings()
+    for instance,settings in jackett_instances.items():
+        INS_NAME = instance
+        JACKETT_URL = settings['url']
+        JACKETT_API = settings['api']
+        ENABLED_INDEXERS = settings['enabled_indexers']
+        JACKETT_ENABLED = settings['enabled']
+        if not JACKETT_ENABLED:
+            logging.debug("{INS_NAME} disabled")
             return []
-    except Exception as e:
-        logging.error(f"Error in scrape_jackett: {str(e)}", exc_info=True)
-        return []
 
-def parse_jackett_results(data: List[Dict[str, Any]]) -> List[Dict[str, Any]]:
+        title = get_title_by_imdb_id(imdb_id)
+        if not title:
+            return []
+
+        year = get_year_from_imdb_id(imdb_id)
+        if not year:
+            return []
+
+        if content_type.lower() == 'movie':
+            params = f"{title} {year}"
+        else:
+            params = f"{title}"
+
+        if content_type.lower() == 'tv' and season is not None:
+            params = f"{params}.s{season:02d}"
+            if episode is not None:
+                params = f"{params}.e{episode:02d}"
+
+        search_endpoint = f"{JACKETT_URL}/api/v2.0/indexers/{JACKETT_FILTER}/results?apikey={JACKETT_API}"
+        
+        query_params = {'Query': params}
+        
+        if ENABLED_INDEXERS:
+            indexers = [indexer.strip().lower() for indexer in ENABLED_INDEXERS.split(',')]
+            query_params.update({f'Tracker[]': indexer for indexer in indexers})
+
+        full_url = f"{search_endpoint}&{urlencode(query_params, doseq=True)}"
+        logging.debug(f"Attempting to access Jackett API with URL: {full_url}")
+
+        try:
+            response = requests.get(full_url, headers={'accept': 'application/json'})
+            logging.debug(f"Jackett API status code: {response.status_code}")
+            if response.status_code == 200:
+                try:
+                    data = response.json()
+                    jackett_results += parse_jackett_results(data["Results"],INS_NAME)
+                except requests.exceptions.JSONDecodeError as json_error:
+                    logging.error(f"Failed to parse JSON response: {str(json_error)}")
+                    return []
+            else:
+                logging.error(f"Jackett API error: Status code {response.status_code}")
+                return []
+        except Exception as e:
+            logging.error(f"Error in scrape_jackett: {str(e)}", exc_info=True)
+            return []
+    return jackett_results
+
+def parse_jackett_results(data: List[Dict[str, Any]],ins_name: str) -> List[Dict[str, Any]]:
     results = []
     for item in data:
         if item.get('MagnetUri'):
@@ -78,9 +82,8 @@ def parse_jackett_results(data: List[Dict[str, Any]]) -> List[Dict[str, Any]]:
             result = {
                 'title': item.get('Title', 'N/A'),
                 'size': item.get('Size', 0) / (1024 * 1024 * 1024),  # Convert to GB
-                'source': f"Jackett - {item.get('Tracker', 'N/A')}",
+                'source': f"{ins_name} - {item.get('Tracker', 'N/A')}",
                 'magnet': magnet
             }
             results.append(result)
-
     return results
