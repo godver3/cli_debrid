351cfbc64760307961967df08eb3a89a26240f96
godver3 <liam.d.hughes@gmail.com>
2024-08-12 21:12:09 +0000

    Removing ignored files

--- helper4.py
+++
@@ -1,9 +0,0 @@
-from trakt.tv import TVShow
-
-# Get the show object for "Sunny"
-sunny = TVShow('Sunny')
-
-# Get the airtime from the airs attribute
-airtime = sunny.airs
-
-print(f"The airtime for Sunny is: {airtime}")
