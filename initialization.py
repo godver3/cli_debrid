b3d020009b4f6c15fc30dd01f4c90f000c2478d2
godver3 <liam.d.hughes@gmail.com>
2024-08-23 00:49:32 -0600

    Fixed init

--- initialization.py
+++ initialization.py
@@ -3,7 +3,7 @@ from metadata.metadata import refresh_release_dates, process_metadata
 from content_checkers.overseerr import get_wanted_from_overseerr
 from content_checkers.mdb_list import get_wanted_from_mdblists
 from content_checkers.trakt import get_wanted_from_trakt_lists, get_wanted_from_trakt_watchlist
-from utilities.plex_functions import get_collected_from_plex
+from utilities.debug_commands import get_and_add_all_collected_from_plex, get_and_add_recent_collected_from_plex
 from utilities.debug_commands import get_all_wanted_from_enabled_sources
 from database import add_wanted_items, add_collected_items, update_media_item_state, get_all_media_items
 from settings import get_setting
@@ -18,45 +18,12 @@ def reset_queued_item_status():
             logging.info(f"Reset item {format_item_log(item)} (ID: {item['id']}) from {state} to Wanted")
 
 def plex_collection_update(skip_initial_plex_update):
-    if skip_initial_plex_update:
-        logging.info("Skipping initial Plex update due to debug flag, completing recently added scan instead.")
-        collected_content = get_collected_from_plex('recent')
-        if collected_content:
-            add_collected_items(collected_content['movies'] + collected_content['episodes'], recent=True)
-        return
     logging.info("Updating Plex collection...")
-    collected_content = get_collected_from_plex('all')
-    if collected_content:
-        add_collected_items(collected_content['movies'] + collected_content['episodes'])
 
-'''
-def overseerr_wanted_update():
-    logging.info("Updating Overseerr wanted items...")
-    
-    wanted_content = get_wanted_from_overseerr()
-    if wanted_content:
-        wanted_content_processed = process_metadata(wanted_content)
-        if wanted_content_processed:
-            add_wanted_items(wanted_content_processed['movies'] + wanted_content_processed['episodes'])
-
-def mdblist_wanted_update():
-    logging.info("Updating MDBList wanted items...")
-    
-    wanted_content = get_wanted_from_mdblists()
-    if wanted_content:
-        wanted_content_processed = process_metadata(wanted_content)
-        if wanted_content_processed:
-            add_wanted_items(wanted_content_processed['movies'] + wanted_content_processed['episodes'])
-            
-def mdblist_wanted_update():
-    logging.info("Updating Trakt wanted items...")
-    
-    wanted_content = get_wanted_from_trakt()
-    if wanted_content:
-        wanted_content_processed = process_metadata(wanted_content)
-        if wanted_content_processed:
-            add_wanted_items(wanted_content_processed['movies'] + wanted_content_processed['episodes'])
-'''
+    if skip_initial_plex_update:
+        get_and_add_recent_collected_from_plex()
+        return
+    get_and_add_all_collected_from_plex()
 
 def format_item_log(item):
     if item['type'] == 'movie':
