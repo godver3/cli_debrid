ba396ea1f2247231a459e1377e7c493051a24559
godver3 <liam.d.hughes@gmail.com>
2024-09-04 21:38:06 -0600

    [0.1.26] Fixed 24 hour toggle and compact toggle

--- routes/__init__.py
+++ routes/__init__.py
@@ -3,7 +3,7 @@ from .models import admin_required, user_required
 from .auth_routes import auth_bp
 from .scraper_routes import scraper_bp
 from .queues_routes import queues_bp
-from .api_summary_routes import api_summary_bp
+from .api_summary_routes import api_summary_bp, real_time_api_bp
 from .onboarding_routes import onboarding_bp
 from .user_management_routes import user_management_bp
 from .database_routes import database_bp
@@ -31,6 +31,7 @@ def register_blueprints(app):
         (logs_bp, '/logs'),
         (settings_bp, '/settings'),
         (program_operation_bp, '/program_operation'),
+        (real_time_api_bp, '/real_time_api'),
     ]
     
     for blueprint, url_prefix in blueprints:
