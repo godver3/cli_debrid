e3026e70f62062892856e2a4544f7a2498ce111c
godver3 <liam.d.hughes@gmail.com>
2024-09-09 14:13:36 -0600

    [0.2.36] Reduce excess logging for debug

--- routes/utils.py
+++ routes/utils.py
@@ -4,5 +4,4 @@ from settings import get_setting
 
 def is_user_system_enabled():
     enable_user_system = get_setting('UI Settings', 'enable_user_system', False)
-    logging.debug(f"UI Settings - enable_user_system: {enable_user_system}")
     return enable_user_system
