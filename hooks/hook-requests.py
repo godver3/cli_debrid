39158f37510f52364e2a7ee7676d880df72d681a
godver3 <liam.d.hughes@gmail.com>
2024-10-05 08:45:15 -0600

    [0.4.95] Initial Windows testing

--- hooks/hook-requests.py
+++ hooks/hook-requests.py
@@ -1,4 +1,4 @@
 from PyInstaller.utils.hooks import collect_submodules, collect_data_files
-  from PyInstaller.utils.hooks import collect_submodules
-  hiddenimports = collect_submodules('requests')
+
+hiddenimports = collect_submodules('requests')
 datas = collect_data_files('requests')
