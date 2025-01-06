1500fbe4b4c889767648ef82c61248b61e89f8e1
godver3 <liam.d.hughes@gmail.com>
2025-01-06 07:11:34 -0700

    Fixed too many sql variables, fixed active download display, fixed pending uncached call, fixed scraper error if missing year, fixed double remove in checking queue, fixed download items being grouped, fixed checking queue functions (needs testing)

--- cli_battery/app/__init__.py
+++ cli_battery/app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from .database import init_db
 import os
+from .limiter import init_limiter
 
 def create_app():
     app = Flask(__name__)
@@ -15,15 +16,18 @@ def create_app():
 
     with app.app_context():
         init_db(app)
+        init_limiter(app)  # Initialize the rate limiter
 
     # Import and register blueprints
     from app.routes.site_routes import main_bp
     from app.routes.api_routes import api_bp
     from app.routes.trakt_routes import trakt_bp
     from app.routes.settings_routes import settings_bp
+    from app.routes.queues_routes import queues_bp
     app.register_blueprint(main_bp)
     app.register_blueprint(api_bp)
     app.register_blueprint(trakt_bp)
     app.register_blueprint(settings_bp)
+    app.register_blueprint(queues_bp)
 
     return app
